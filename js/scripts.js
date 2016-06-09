49ef861d501d78b5db3d243151fe8194531e1d5b
Dan Siegel and Marissa Siegel <dan.j.siegel@gmail.com>
2016-06-08 20:28:38 -0700

    commit we will practice rename to functions all working

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,6 @@
-$(document).ready(function() {
-  $("form#triangle").submit(function() {
-    event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var number3 = parseInt($("#input3").val());
-    if (equilateral(number1, number2, number3)) {
-      var result = 'equilateral';
-    } else if (isosceles(number1, number2, number3)) {
-      var result = 'isosceles';
-    } else if (scalene(number1, number2, number3)) {
-      var result = 'scalene';
-    }
-    $("#type").text(result);
-});
-});
-
+var not_triangle = function (number1, number2, number3) {
+  return (number1 + number2 <= number3 || number2 + number3 <= number1 || number1 + number3 <= number2);
+};
 var equilateral = function(number1, number2, number3) {
   return (number1 === number2 && number1 === number3);
 };
@@ -24,7 +10,22 @@ var isosceles = function (number1, number2, number3) {
 var scalene = function (number1, number2, number3) {
   return (number1 !== number2 && number2 !== number3 && number1 !== number3);
 };
-var not-triangle = function (number1, number2, number3) {
-  return ()
 
-}
+$(document).ready(function() {
+  $("form#triangle").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var number3 = parseInt($("#input3").val());
+    if (not_triangle(number1, number2, number3)){
+      var result = 'not a triangle';
+    } else if (equilateral(number1, number2, number3)) {
+      var result = 'equilateral';
+    } else if (scalene(number1, number2, number3)) {
+      var result = 'scalene';
+    } else if (isosceles(number1, number2, number3)) {
+      var result = 'isosceles';
+    }
+    $("#type").text(result);
+});
+});
