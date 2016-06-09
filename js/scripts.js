c31c2bbc800b1efb8997f6348482d108c6550dc2
Dan Siegel and Marissa Siegel <dan.j.siegel@gmail.com>
2016-06-08 18:47:41 -0700

    display functioning calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -15,13 +16,30 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+
 $(document).ready(function() {
   $("form#calculator").submit(function() {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
+    var operator = $("input:radio[name=operator]:checked").val()
+    if (operator == 'add')
+    {
     var result = add(number1, number2);
     $("#output").text(result);
+  } else if (operator == 'subtract')
+   {
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  } else if (operator == 'multiply')
+     {
+      var result = multiply(number1, number2);
+      $("#output").text(result);
+  } else if (operator == 'divide')
+      {
+      var result = divide(number1, number2);
+      $("#output").text(result);
+      }
+      
   });
 });
