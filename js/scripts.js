345067a2452ee0701e722e6e47dc554533da6d81
Dan Siegel and Marissa Siegel <mrz_87@yahoo.com>
2016-06-08 20:15:36 -0700

    display a triangle type website

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,30 @@
-
-
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-
 $(document).ready(function() {
-  $("form#calculator").submit(function() {
+  $("form#triangle").submit(function() {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val()
-    if (operator == 'add')
-    {
-    var result = add(number1, number2);
-    $("#output").text(result);
-  } else if (operator == 'subtract')
-   {
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  } else if (operator == 'multiply')
-     {
-      var result = multiply(number1, number2);
-      $("#output").text(result);
-  } else if (operator == 'divide')
-      {
-      var result = divide(number1, number2);
-      $("#output").text(result);
-      }
-      
-  });
+    var number3 = parseInt($("#input3").val());
+    if (equilateral(number1, number2, number3)) {
+      var result = 'equilateral';
+    } else if (isosceles(number1, number2, number3)) {
+      var result = 'isosceles';
+    } else if (scalene(number1, number2, number3)) {
+      var result = 'scalene';
+    }
+    $("#type").text(result);
 });
+});
+
+var equilateral = function(number1, number2, number3) {
+  return (number1 === number2 && number1 === number3);
+};
+var isosceles = function (number1, number2, number3) {
+  return (number1 === number2 || number2 === number3);
+};
+var scalene = function (number1, number2, number3) {
+  return (number1 !== number2 && number2 !== number3 && number1 !== number3);
+};
+var not-triangle = function (number1, number2, number3) {
+  return ()
+
+}
